b3bbb080e6c258c52c3a9334c561b8b3c5461a46
Daniel Jabaraj V <danieljabaraj218@gmail.com>
2024-10-12 12:13:54 +0530

    Create Arr.java

---
+++ Arr.java
@@ -0,0 +1,37 @@
+public class Arr{
+    public static void main(String [] args){
+        System.out.println("Number of Days in all Months");
+        int month_days[];
+        month_days = new int[12];
+        month_days[0] = 31;
+        month_days[1] = 28;
+        month_days[2] = 31;
+        month_days[3] = 30;
+        month_days[4] = 31;
+        month_days[5] = 30;
+        month_days[6] = 31;
+        month_days[7] = 31;
+        month_days[8] = 30;
+        month_days[9] = 31;
+        month_days[10] = 30;
+        month_days[11] = 31;
+
+        String month[] = new String[12];
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+
+        for ( int i = 0; i < 12 ; i++){
+            System.out.println(month[i] +" has "+ month_days[i] + " days");
+        }
+    }
+}
